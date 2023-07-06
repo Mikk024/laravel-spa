79f3e97ec985992280654407361be164f6606a7e
Mikolaj Godek <mikolajgodek024@gmail.com>
2023-07-06 15:57:08 +0200

    create models and migrations

--- backend/database/migrations/2023_07_06_132159_create_reservations_table.php
+++ backend/database/migrations/2023_07_06_132159_create_reservations_table.php
@@ -11,16 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reservations', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignUuid('user_id')->constrained('users');
+            $table->foreignUuid('room_id')->constrained('rooms');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->integer('price');
+            $table->integer('total');
             $table->timestamps();
-            $table->integer('phone_number');
-            $table->string('profile_image');
         });
     }
 
@@ -29,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reservations');
     }
 };
--- backend/database/migrations/2023_07_06_132209_create_reviews_table.php
+++ backend/database/migrations/2023_07_06_132209_create_reviews_table.php
@@ -11,16 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reviews', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignUuid('reservation_id')->constrained('reservations');
+            $table->integer('rating');
+            $table->string('comment');
             $table->timestamps();
-            $table->integer('phone_number');
-            $table->string('profile_image');
         });
     }
 
@@ -29,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reviews');
     }
 };
