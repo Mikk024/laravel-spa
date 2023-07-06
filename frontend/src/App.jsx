ede095e1902cb80a1a77d42b71270ec3890d6f40
Mikolaj Godek <mikolajgodek024@gmail.com>
2023-07-06 15:15:47 +0200

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl text-blue-500 text-center mt-10">chuj</h1>
+    </>
+  )
+}
+
+export default App
